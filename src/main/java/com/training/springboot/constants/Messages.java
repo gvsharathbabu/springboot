43a22741d0a002f6bfbf9fa83e9863dbb4e37e9e
gvsharathbabu <gvbabu@deloitte.com>
2022-09-29 12:02:29 +0530

    Screenshots of junit and junit testing added

--- src/main/java/com/training/springboot/constants/Messages.java
+++ src/main/java/com/training/springboot/constants/Messages.java
@@ -4,6 +4,5 @@ public class Messages {
 
     public static final String BIRTH_DATE_INVALID = "The birth date must be greater or equal to 18";
     public static final String USER_INVALID = "User details must be not blank";
-
     public static final String USER_VALID = "User saved successfully";
 }
--- src/test/java/com/training/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/training/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,48 @@
 package com.training.springboot;
 
+import com.training.springboot.constants.Messages;
+import com.training.springboot.dto.Address;
+import com.training.springboot.dto.User;
+import com.training.springboot.exceptions.UnableToProcessException;
+import com.training.springboot.service.UserValidationImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @InjectMocks
+    private UserValidationImpl userValidation;
 
+    @Test
+    void validScenario() {
+
+        Address address = new Address(1L, "street",
+                "city", "state", "zipcode", "country");
+
+        User user = new User(1L, "test@email.com", LocalDate.of(1996, 6, 6), "M",
+                new BigDecimal(100), address);
+        Assertions.assertEquals(Messages.USER_VALID, userValidation.validateUser(user));
+    }
+
+    @Test
+    void inValidAgeScenario() {
+
+        Address address = new Address(1L, "street",
+                "city", "state", "zipcode", "country");
+
+        User user = new User(1L, "test@email.com", LocalDate.of(2022, 6, 6), "M",
+                new BigDecimal(100), address);
+        UnableToProcessException thrown = Assertions
+                .assertThrows(UnableToProcessException.class,
+                        () -> userValidation.validateUser(user),
+                        Messages.BIRTH_DATE_INVALID);
+
+        Assertions.assertEquals(Messages.BIRTH_DATE_INVALID, thrown.getMessage());
+    }
 }
